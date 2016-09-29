2e15afacee850d333362db18d163081b3af1292e
Melissa <sc14ml@leeds.ac.uk>
2016-09-29 17:28:08 +0100

    Added a load of code
    And this is to test to see if descriptions work.

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -1,12 +1,13 @@
 import java.io.*;
 
-public class Pokedex
+public class Pokedex extends ArrayList<Pokemon>
 {
     private String workingdir = System.getProperty("user.dir");
     
     public Pokedex()
     {
-
+        super();
+        readFile();
     }
 
     /*
@@ -16,7 +17,7 @@ public class Pokedex
     }
     */
 
-    private void readFile(String filename)
+    private void readFile()
     {
         String statsfile = this.workingdir.substring(0,this.workingdir.length()-4).concat("\\res\\stats.csv");
         BufferedReader br = null;
@@ -52,57 +53,16 @@ public class Pokedex
     private Pokemon dataConversion()
     {
         String[] pokemonArray = line.split(",");
-        // Create pokemon here from all fields
-        // 0 ndex
-        // 1 pokemon
-        // 2 primarytype
-        // 3 secondarytype
-        // 4 evolutionarystage
-        // 5 pace
-        // 6 agility
-        // 7 smarts
-        // 8 strength
-        // 9 spirit
-        // 10 vigor 
-        // 11 weak
-        // 12 strong
-        // 13 small
-        // 14 large
-        // 15 charisma - calculated
-        // 16 parry - calculated 
-        // 17 toughness - toughness
-        // 18 intimidation
-        // 19 notice
-        // 20 stealth
-        // 21 survival
-        // 22 swimming
-        // 23 fighting
-        // 24 spAttack
-        // 25 climbing
-        // 26 healing
-        // 27 taunting
-        // 28 tracking
-        // 29 charm
-        // 30 edges            armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
-        // 34 hindrances       always aquatic, rage, ugly
-        // 36 inArctic
-        // 37 inCaves
-        // 38 inDesert
-        // 39 inFreshWater
-        // 40 inMountains
-        // 41 inSea
-        // 42 inPlains
-        // 43 inUrban
-        // 44 inVolcanic
-        // 45 inWoods
-        // 46 rareness
-        // 47 maxgroupsize
-        // 48 aggressiveness
-        // 49 pokemonFamily
     }
+
+
+    /****************************************
+        MAIN FOR TESTING
+    ****************************************/
 
     public static void main(String[] args)
     {
-
+        Pokemon test = new Pokemon();
     }
+
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,52 +4,146 @@ public class Pokemon
 {
     private String workingdir = System.getProperty("user.dir");
     
-        public Integer ndex
-        public String pokemon
-        public Type primaryType = null;
-        public Type secondaryType = null;
-        public Integer evolutionarystage
-        public Integer pace
-        public Integer agility
-        public Integer smarts
-        public Integer strength
-        public Integer spirit
-        public Integer vigor 
-        public Boolean weak
-        public Boolean strong
-        public Boolean small
-        public Boolean large
-        public Integer charisma                     // calculated
-        public Integer parry                        // calculated 
-        public Integer toughness                    // calculated
-        public Integer Integerimidation
-        public Integer notice
-        public Integer stealth
-        public Integer survival
-        public Integer swimming
-        public Integer fighting
-        public Integer spAttack
-        public Integer climbing
-        public Integer healing
-        public Integer taunting
-        public Integer tracking
-        public Integer charm
-        public Integer edges            // armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
-        public Integer hindrances       // always aquatic, rage, ugly
-        public Boolean inArctic
-        public Boolean inCaves
-        public Boolean inDesert
-        public Boolean inFreshWater
-        public Boolean inMountains
-        public Boolean inSea
-        public Boolean inPlains
-        public Boolean inUrban
-        public Boolean inVolcanic
-        public Boolean inWoods
-        public Boolean rareness
-        public Boolean maxgroupsize
-        public Boolean aggressiveness
-        public Boolean pokemonFamily
+        public Integer ndex;
+        public String pokemon;
+        public Type primaryType;
+        public Type secondaryType;
+        public Integer evolutionaryStage;
+        public Integer pace;
+        public Attribute agility;
+        public Attribute smarts;
+        public Attribute strength;
+        public Attribute spirit;
+        public Attribute vigor;
+        public PhysicalAttribute weak;
+        public PhysicalAttribute strong;
+        public PhysicalAttribute small;
+        public PhysicalAttribute large;
+        public Attribute charisma;                    // calculated
+        public Attribute parry;                       // calculated 
+        public Attribute toughness;                   // calculated
+        public Skill imtidation;
+        public Skill notice;
+        public Skill stealth;
+        public Skill survival;
+        public Skill swimming;
+        public Skill fighting;
+        public Skill spAttack;
+        public Skill climbing;
+        public Skill healing;
+        public Skill taunting;
+        public Skill tracking;
+        public Skill charm;
+        public Skill[] edges = new Skill[4];            // armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
+        public Skill[] hindrances = new Skill[2];     // always aquatic, rage, ugly
+        public Location inArctic;
+        public Location inCaves;
+        public Location inDesert;
+        public Location inFreshWater;
+        public Location inMountains;
+        public Location inSea;
+        public Location inPlains;
+        public Location inUrban;
+        public Location inVolcanic;
+        public Location inWoods;
+        public Integer rareness;
+        public Integer maxGroupSize;
+        public Integer aggro;
+        public String pokemonFamily;
+
+        public void create(ArrayList<E> listIn)
+        {
+            this.ndex = Integer.parseInt(listIn[0]);
+            this.pokemon = listIn[1];
+            this.primaryType = new Type(true, listIn[2]);
+            this.secondaryType = new Type(false, listIn[3]);
+            this.evolutionaryStage = Integer.parseInt(listIn[4]);
+            this.pace = Integer.parseInt(listIn[5]);
+
+            this.agility = new Attribute(Integer.parseInt(ListIn[6]), 0);
+            this.smarts = new Attribute(Integer.parseInt(ListIn[7]), 0);
+            this.strength = new Attribute(Integer.parseInt(ListIn[8]), 0);
+            this.spirit = new Attribute(Integer.parseInt(ListIn[9]), 0);
+            this.vigor = new Attribute(Integer.parseInt(ListIn[10]), 0);
+
+            this.weak = new PhysicalAttribute(convertIntToBool(Integer.parseInt(ListIn[11])));
+            this.strong = new PhysicalAttribute(convertIntToBool(Integer.parseInt(ListIn[12])));
+            this.small = new PhysicalAttribute(convertIntToBool(Integer.parseInt(ListIn[13])));
+            this.large = new PhysicalAttribute(convertIntToBool(Integer.parseInt(ListIn[14])));
+
+            this.intimidation = new Skill(Integer.parseInt(ListIn[18]));
+            this.notice = new Skill(Integer.parseInt(ListIn[19]));
+            this.stealth = new Skill(Integer.parseInt(ListIn[20]));
+            this.survival = new Skill(Integer.parseInt(ListIn[21]));
+            this.swimming = new Skill(Integer.parseInt(ListIn[22]));
+            this.fighting = new Skill(Integer.parseInt(ListIn[23]));
+            this.spAttack = new Skill(Integer.parseInt(ListIn[24]));
+            this.climbing = new Skill(Integer.parseInt(ListIn[25]));
+            this.healing = new Skill(Integer.parseInt(ListIn[26]));
+            this.taunting = new Skill(Integer.parseInt(ListIn[27]));
+            this.tracking = new Skill(Integer.parseInt(ListIn[28]));
+            this.charm = new Skill(Integer.parseInt(ListIn[29]));
+
+            this.inArctic = new Location(convertIntToBool(Integer.parseInt(ListIn[36])));
+            this.inCaves = new Location(convertIntToBool(Integer.parseInt(ListIn[37])));
+            this.inDesert = new Location(convertIntToBool(Integer.parseInt(ListIn[38])));
+            this.inFreshWater = new Location(convertIntToBool(Integer.parseInt(ListIn[39])));
+            this.inMountains = new Location(convertIntToBool(Integer.parseInt(ListIn[40])));
+            this.inSea = new Location(convertIntToBool(Integer.parseInt(ListIn[41])));
+            this.inPlains = new Location(convertIntToBool(Integer.parseInt(ListIn[42])));
+            this.inUrban = new Location(convertIntToBool(Integer.parseInt(ListIn[43])));
+            this.inVolcanic = new Location(convertIntToBool(Integer.parseInt(ListIn[44])));
+            this.inWoods = new Location(convertIntToBool(Integer.parseInt(ListIn[45])));
+
+            this.rareness = Integer.parseInt(listIn[46]);
+            this.maxGroupSize = Integer.parseInt(listIn[47]);
+            this.aggro = Integer.parseInt(listIn[48]);
+            this.pokemonFamily = listIn[49];
+
+            setEdges();
+            setHindrance();
+            calculateCPT();
+        }
+
+        private Boolean convertIntToBool(Integer intIn)
+        {
+            if (intIn == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void setEdges()
+        {
+            for (int i=0; i<4; i++)
+            {
+                this.edges[i] = null;
+            }
+
+        }
+
+        private void setHindrances()
+        {
+            for (int j=0; j<2; j++)
+            {
+                this.hindrances = null;
+            }
+
+        }
+
+        private void calculateCPT()
+        {
+            //calculate charisma, parry, toughness here
+        }
+
+
+        /****************************************
+            SUBCLASSES
+        ****************************************/
 
         public class Type
         {
@@ -76,13 +170,11 @@ public class Pokemon
 
         public class Attribute
         {
-            private String attribute = "";
             private Integer diceNumber = 0;
             private Integer modifier = 0;
 
-            public Attribute(String attributeIn, Integer diceIn, Integer modifierIn)
+            public Attribute(Integer diceIn, Integer modifierIn)
             {
-                attribute = attributeIn;
                 diceNumber = diceIn;
                 modifer = modifierIn;
             }
@@ -97,12 +189,12 @@ public class Pokemon
                 return this.modifier;
             }
 
-            public setDice(Integer diceIn)
+            public void setDice(Integer diceIn)
             {
                 this.diceNumber = diceIn;
             }
 
-            public setModifier(Integer modifierIn)
+            public void setModifier(Integer modifierIn)
             {
                 this.modifier = modifierIn;
             }
@@ -110,24 +202,72 @@ public class Pokemon
 
         public class PhysicalAttribute
         {
-            private String attribute = "";
             private Boolean isSet;
 
-            public Attribute(String attributeIn, Integer diceIn, Integer modifierIn)
+            public PhysicalAttribute(Boolean boolIn)
             {
-                attribute = attributeIn;
+                isSet = boolIn;
+            }
+
+            public void set(Bool boolin)
+            {
+                isSet = boolIn;
+            }
+
+            public Boolean isSet()
+            {
+                return this.isSet;
+            }
+        }
+
+        public class Skill
+        {
+            private Integer diceNumber = 0;
+            private Integer modifier = 0;
+
+            public Skill(Integer diceIn, Integer modifierIn)
+            {
                 diceNumber = diceIn;
                 modifer = modifierIn;
             }
 
-            public setDice(Integer diceIn)
+            public Integer getDice()
+            {
+                return this.diceNumber;
+            }
+
+            public Integer getModifier()
+            {
+                return this.modifier;
+            }
+
+            public void setDice(Integer diceIn)
             {
                 this.diceNumber = diceIn;
             }
 
-            public setModifier(Integer modifierIn)
+            public void setModifier(Integer modifierIn)
             {
                 this.modifier = modifierIn;
+            }
+        }
+
+        public class Location
+        {
+            private Boolean spawnsHere;
+            public Location(Boolean boolIn)
+            {
+                spawnsHere = boolIn;
+            }
+
+            public void setSpawn(Boolean boolIn)
+            {
+                this.spawnsHere = boolIn;
+            }
+
+            public Boolean spawnsHere()
+            {
+                return this.spawnsHere;
             }
         }
 
