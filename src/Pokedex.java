0190d8f0fb6df57af16c2a7a20b318edc5d13d2c
Melissa <sc14ml@leeds.ac.uk>
2016-09-29 15:33:14 +0100

    Started on Pokemon class.  \n Currently up to PhysicalAttribute subclass, and writing getters and setters. Yay, subclasses.

--- src/Pokedex.java
+++ src/Pokedex.java
@@ -16,7 +16,7 @@ public class Pokedex
     }
     */
 
-    private readFile(String filename)
+    private void readFile(String filename)
     {
         String statsfile = this.workingdir.substring(0,this.workingdir.length()-4).concat("\\res\\stats.csv");
         BufferedReader br = null;
@@ -49,47 +49,56 @@ public class Pokedex
         
     }
 
-    private dataConversion()
+    private Pokemon dataConversion()
     {
         String[] pokemonArray = line.split(",");
         // Create pokemon here from all fields
-        // ndex
-        // pokemon
-        // primarytype
-        // secondarytype
-        // evolutionarystage
-        // pace
-        // agility
-        // smarts
-        // strength
-        // spirit
-        // vigor 
-        // weak
-        // strong
-        // small
-        // large
-        // charisma
-        // parry
-        // toughness      
-        // intimidation
-        // notice
-        // stealth
-        // survival
-        // swimming
-        // fighting
-        // spAttack
-        // climbing
-        // healing
-        // taunting
-        // tracking
-        // charm
-        // edges            armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
-        // hindrances       always aquatic, rage, ugly
-        // environments
-        // rareness
-        // maxgroupsize
-        // aggressiveness
-        // pokemonFamily
+        // 0 ndex
+        // 1 pokemon
+        // 2 primarytype
+        // 3 secondarytype
+        // 4 evolutionarystage
+        // 5 pace
+        // 6 agility
+        // 7 smarts
+        // 8 strength
+        // 9 spirit
+        // 10 vigor 
+        // 11 weak
+        // 12 strong
+        // 13 small
+        // 14 large
+        // 15 charisma - calculated
+        // 16 parry - calculated 
+        // 17 toughness - toughness
+        // 18 intimidation
+        // 19 notice
+        // 20 stealth
+        // 21 survival
+        // 22 swimming
+        // 23 fighting
+        // 24 spAttack
+        // 25 climbing
+        // 26 healing
+        // 27 taunting
+        // 28 tracking
+        // 29 charm
+        // 30 edges            armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
+        // 34 hindrances       always aquatic, rage, ugly
+        // 36 inArctic
+        // 37 inCaves
+        // 38 inDesert
+        // 39 inFreshWater
+        // 40 inMountains
+        // 41 inSea
+        // 42 inPlains
+        // 43 inUrban
+        // 44 inVolcanic
+        // 45 inWoods
+        // 46 rareness
+        // 47 maxgroupsize
+        // 48 aggressiveness
+        // 49 pokemonFamily
     }
 
     public static void main(String[] args)
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,9 +4,133 @@ public class Pokemon
 {
     private String workingdir = System.getProperty("user.dir");
     
+        public Integer ndex
+        public String pokemon
+        public Type primaryType = null;
+        public Type secondaryType = null;
+        public Integer evolutionarystage
+        public Integer pace
+        public Integer agility
+        public Integer smarts
+        public Integer strength
+        public Integer spirit
+        public Integer vigor 
+        public Boolean weak
+        public Boolean strong
+        public Boolean small
+        public Boolean large
+        public Integer charisma                     // calculated
+        public Integer parry                        // calculated 
+        public Integer toughness                    // calculated
+        public Integer Integerimidation
+        public Integer notice
+        public Integer stealth
+        public Integer survival
+        public Integer swimming
+        public Integer fighting
+        public Integer spAttack
+        public Integer climbing
+        public Integer healing
+        public Integer taunting
+        public Integer tracking
+        public Integer charm
+        public Integer edges            // armed, aquatic, burrowing, cute, double attack, flight, hard, majestic, reach, roch hard, speedy flight, teleporation, transforms
+        public Integer hindrances       // always aquatic, rage, ugly
+        public Boolean inArctic
+        public Boolean inCaves
+        public Boolean inDesert
+        public Boolean inFreshWater
+        public Boolean inMountains
+        public Boolean inSea
+        public Boolean inPlains
+        public Boolean inUrban
+        public Boolean inVolcanic
+        public Boolean inWoods
+        public Boolean rareness
+        public Boolean maxgroupsize
+        public Boolean aggressiveness
+        public Boolean pokemonFamily
 
-    public static void main(String[] args)
-    {
+        public class Type
+        {
+            private Boolean primaryBool = null;
+            private String type = "";
 
-    }
+            public Type(Boolean primaryIn, String typeIn)
+            {
+                primaryBool = primaryIn;
+                type = typeIn;
+            }
+
+            public Boolean isPrimary()
+            {
+                return this.primaryBool;
+            }
+
+            public String getType()
+            {
+                return this.type;
+            }
+
+        } 
+
+        public class Attribute
+        {
+            private String attribute = "";
+            private Integer diceNumber = 0;
+            private Integer modifier = 0;
+
+            public Attribute(String attributeIn, Integer diceIn, Integer modifierIn)
+            {
+                attribute = attributeIn;
+                diceNumber = diceIn;
+                modifer = modifierIn;
+            }
+
+            public Integer getDice()
+            {
+                return this.diceNumber;
+            }
+
+            public Integer getModifier()
+            {
+                return this.modifier;
+            }
+
+            public setDice(Integer diceIn)
+            {
+                this.diceNumber = diceIn;
+            }
+
+            public setModifier(Integer modifierIn)
+            {
+                this.modifier = modifierIn;
+            }
+        }
+
+        public class PhysicalAttribute
+        {
+            private String attribute = "";
+            private Boolean isSet;
+
+            public Attribute(String attributeIn, Integer diceIn, Integer modifierIn)
+            {
+                attribute = attributeIn;
+                diceNumber = diceIn;
+                modifer = modifierIn;
+            }
+
+            public setDice(Integer diceIn)
+            {
+                this.diceNumber = diceIn;
+            }
+
+            public setModifier(Integer modifierIn)
+            {
+                this.modifier = modifierIn;
+            }
+        }
+
+
+    
 }
